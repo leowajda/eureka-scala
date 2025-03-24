bba22d97967ecdec28a67ebd8e97a96c880e8d59
Leonardo Wajda <dev.leowajda@tuta.io>
2025-03-24 13:15:29 +0100

    refactor: pre-compute sliding window

--- src/main/scala/string/recursive/FindAllAnagramsInAString.scala
+++ src/main/scala/string/recursive/FindAllAnagramsInAString.scala
@@ -7,23 +7,24 @@ object FindAllAnagramsInAString:
 
   extension (s: String)
 
-    def characterCount: Array[Int] =
+    def characterCount(end: Int): Array[Int] =
       val counter = new Array[Int](26)
-      s.foreach(ch => counter(ch - 'a') += 1)
+      (0 until end).foreach(idx => counter(s(idx) - 'a') += 1)
       counter
 
   def findAnagrams(s: String, p: String): List[Int] =
 
-    val anagramCount  = p.characterCount
+    if p.length > s.length then return Nil
+
     val anagramSize   = p.length
-    val slidingWindow = new Array[Int](26)
+    val anagramCount  = p.characterCount(anagramSize)
+    val slidingWindow = s.characterCount(anagramSize - 1)
 
     @tailrec def loop(left: Int, right: Int, anagrams: List[Int]): List[Int] =
       if right >= s.length then return anagrams
       slidingWindow(s(right) - 'a') += 1
-      if right < anagramSize - 1 then return loop(left, right + 1, anagrams)
       val isValidAnagram = java.util.Arrays.equals(slidingWindow, anagramCount)
       slidingWindow(s(left) - 'a') -= 1
       loop(left + 1, right + 1, if isValidAnagram then left :: anagrams else anagrams)
 
-    loop(0, 0, Nil)
+    loop(0, anagramSize - 1, Nil)
