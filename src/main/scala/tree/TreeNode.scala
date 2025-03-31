75c110c2d7c52071bd8acf4d86eaf7b948ee42e2
Leonardo Wajda <dev.leowajda@tuta.io>
2025-03-31 12:26:59 +0200

    chore: add TreeNode class

---
+++ src/main/scala/tree/TreeNode.scala
@@ -0,0 +1,8 @@
+package com.eureka
+package tree
+
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null):
+
+  var value: Int      = _value
+  var left: TreeNode  = _left
+  var right: TreeNode = _right
